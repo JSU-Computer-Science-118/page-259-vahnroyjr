92bab8b86838c4b47f6a81539ac7ce65ca073227
vahnroyjr <117847144+vahnroyjr@users.noreply.github.com>
2022-11-09 12:46:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// write your code here
 #include <iostream>
 #include <cmath>
 
@@ -8,26 +9,30 @@ int main()
   double a,b,c,d;
     cout<<"Enter the value of a (coefficient"<<"of square of X):";
   cin>>a;
-  cout<<"Enter the variable of b (coefficient"<<"of X):";
+  if(a==0)
+      cout<<"Invalid entry";
+  else {
+      cout << "Enter the variable of b (coefficient" << "of X):";
+  }
   cin>>b;
     cout<<"Enter the constant term c:";
   cin>>c;
   d=((pow(b,2))-(4*a*c));
   if(d<0)
-    cout<<"the roots are imaginary."<<endl;
+    cout<<"Complex roots"<<endl;
   else
   {
     if(d==0)
     {
-      cout<<"The roots are real and equal"<<endl;
-    cout<<"the roots are"<<(-b/(2*a))<<"and"<<(-b/(2*a))<<endl;
+    cout<<"the single root is "<<(-b/(2*a))<<endl;
   }
     else
   {
-    cout<<"The roots are real and unequal.\n";
+    cout<<"Two real roots\n";
     d=pow(d, 0.5);
-    cout<<"The roots are"<<((-b+d)/(2*a))<<"and"<<((-b-d)/(2*a))<<endl;
+    cout<<"- First root "<<((-b+d)/(2*a))<<endl;
+    cout<<"- Second root "<<((-b-d)/(2*a))<<endl;
   }
 }
   return 0;
-}  
+}
