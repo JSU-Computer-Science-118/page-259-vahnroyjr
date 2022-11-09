f6da978eaebfd8e7a2249c09de17cc3438646eab
vahnroyjr <117847144+vahnroyjr@users.noreply.github.com>
2022-11-09 11:15:02 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,43 @@
 // write your code here
+#include <iostream>
+#include <math>
+
+using namespace std;
+
+int main()
+{
+  double a,b,c,d
+    cout<<"Enter the value of a (coefficent"
+    <<"of square of X):";
+  cin<<a;
+  cout<<"Enter the variable of b (coefficent"
+    <<"of X):";
+  cin>>b
+    cout<<"Enter the constant term c:";
+  cin>>c;
+  d=((pow(b,2))-(4*a*c));
+  if(d<0)
+    cout<<"the roots are imaginary."<<endl;
+  else
+  {
+    if(d==0)
+      cout<<"The roots are real and equal"
+      <<endl;
+    cout<<"the roots are"<<(-b/2*a))
+      <<"and"<<(-b/2*a))<<endl;
+  }
+  else
+  {
+    cout<<"The roots are real and unequal.\n";
+    d=pow(d, 0.5);
+    cout<<"The roots are"
+      <<((-b+d)/(2*a))
+      <<"and"<<((-b-d)/(2*a))
+      <<endl;
+  }
+}
+system("pause")
+  return 0;
+}
+    
+    
